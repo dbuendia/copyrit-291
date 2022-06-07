24b6121c55c3eb3cbba4fa32e42f994b88714df0
Javier Rivera <arq.javierriver@gmail.com>
2022-06-07 19:56:12 +0200

    javis feedback

--- src/asynchronism/fetchChallenge.js
+++ src/asynchronism/fetchChallenge.js
@@ -5,22 +5,26 @@ Crea una funcion, getUser(id) la cual toma un id (un numero) y muestra por conso
 */
 
 let endpointUsers = "https://jsonplaceholder.typicode.com/users/";
+// Instead we use a BASE URL
+const BASE_URL = "https://jsonplaceholder.typicode.com";
 
 function getUser(id) {
   // Comprobamos que id sea un número
   if (typeof id === "number") {
-    fetch(endpointUsers + id)
-      .then((response) => response.json())
-      .then((data) => {
+    const USER_URL = `${BASE_URL}/users/${id}`;
+    fetch(USER_URL)
+      .then(response => response.json())
+      .then(data => {
         return console.log(data);
       })
-      .catch((error) => console.log(error));
+      .catch(error => console.log(error));
   } else {
     console.log("No se ha indicado un número como id de usuario");
   }
 }
 
-// getUser(1);
+console.log("get users callback");
+getUser(1);
 // getUser(5);
 // getUser("1");
 
@@ -31,22 +35,24 @@ Revisa la documentación de https://jsonplaceholder.typicode.com/ para identific
 
 */
 
-endpointPosts = "https://jsonplaceholder.typicode.com/posts/";
-
 function getPosts(id) {
+  const POST_URL = `${BASE_URL}/posts/${id}`;
+  const ALL_POSTS_URL = `${BASE_URL}/posts/`;
+
   if (id === undefined) {
-    fetch(endpointPosts)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+    fetch(ALL_POSTS_URL)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error));
   } else {
-    fetch(endpointPosts + id)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+    fetch(POST_URL)
+      .then(response => response.json())
+      .then(data => console.dir(data))
+      .catch(error => console.log(error));
   }
 }
 
+console.log("get POSTS callback");
 getPosts(5);
 getPosts(); // Así podría querer mostrar todos los posts.
 
@@ -59,21 +65,27 @@ Devuelve todos los usuarios o todos los posts.
 */
 
 function getResource(resource) {
+  let RESOURCE_URL = "";
+
   if (resource === "users") {
-    fetch(endpointUsers)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+    RESOURCE_URL = `${BASE_URL}/users`;
   } else if (resource === "posts") {
-    fetch(endpointPosts)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+    RESOURCE_URL = `${BASE_URL}/posts/`;
   } else {
-    console.log(resource + " no es uno de los parámetros esperados.");
+    console.error(resource + " no es uno de los parámetros esperados.");
+    return;
   }
+
+  console.log("RESOURCE_URL :>> ", RESOURCE_URL);
+
+  fetch(RESOURCE_URL)
+    .then(response => response.json())
+    .then(data => console.table(data))
+    .catch(error => console.error(error));
 }
 
 getResource("users");
 getResource("posts");
 getResource("What a country");
+
+console.info({ prop: "HEY GUESS WHAT MIMI" });
